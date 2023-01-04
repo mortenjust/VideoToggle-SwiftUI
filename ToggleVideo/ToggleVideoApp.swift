fbb7a04f1351cdefc3a71a8abc1d432f29022b20
Morten Just <morten.just@genstart.dk>
2023-01-04 20:36:19 +0100

    Initial Commit

---
+++ ToggleVideo/ToggleVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleVideoApp.swift
+//  ToggleVideo
+//
+//  Created by Morten Just on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToggleVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
